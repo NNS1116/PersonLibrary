7f85c6fbb4f57f85f16597142548026b5f0f77f1
Nahlan <nahlan-nahla@outlook.com>
2024-09-13 03:37:50 -0400

    Initial commit

---
+++ PersonLibrary/Person.cs
@@ -0,0 +1,7 @@
+﻿namespace PersonLibrary
+{
+    public class Person
+    {
+        
+    }
+}
---
+++ PersonLibrary/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonLibrary
+{
+    public class Student : Person
+    {
+        public string GetCellPhoneNumber()
+        {
+            // Returning a placeholder phone number
+            return "123-456-7890";
+        }
+    }
+}
+
